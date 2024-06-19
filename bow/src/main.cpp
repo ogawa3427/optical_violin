c97cf97435b882d565e0d54275b7c4d5abc6f6eb
Ogawa <135859092+ogawa3427@users.noreply.github.com>
2024-06-20 05:30:38 +0900

    上手く割り込めない

--- bow/src/main.cpp
+++ bow/src/main.cpp
@@ -1,5 +1,9 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include <Encoder.h>
+
+// エンコーダのピン番号を指定
+Encoder myEnc(5, 6);
 
 static int toneNum = 0;
 static int lasttoneNum = 0;
@@ -16,6 +20,16 @@ M5UnitSynth synth;
 
 int instrument = 41;
 
+void updateEncoder() {
+  static long lastPosition = -999;
+  long newPosition = myEnc.read();
+  if (newPosition != lastPosition) {
+    lastPosition = newPosition;
+    USBSerial.println(newPosition);
+  }
+}
+
+
 void setup()
 {
   Wire.begin();          // I2Cを初期化
@@ -25,208 +39,56 @@ void setup()
   synth.setNoteOn(0, NOTE_C6, 127);
   delay(1000);
   synth.setNoteOff(0, NOTE_C6, 0);
+  // 割り込みの設定
+  attachInterrupt(digitalPinToInterrupt(5), updateEncoder, CHANGE);
+  // attachInterrupt(digitalPinToInterrupt(6), updateEncoder, CHANGE);
 }
 
 void loop()
 {
-  line = 'E'; //'G','D', 'A', 'E'
-  // 1/100秒で変わることはちょっと忘れさせてください
-  Wire.requestFrom(0x08, 4);
-  uint8_t loopCount = 0;
-  while (Wire.available())
-  {
-    uint8_t c = Wire.read();
-    if (loopCount == 3)
-    {
-      USBSerial.println(c, BIN);
-      if ((c & 0b1000) == 0b1000)
-      {
-        sw_R = true;
-      } 
-      else {
-        sw_R = false;
-      }
-      if (c >= 0b10000 && c < 0b100000)
-      {
-        sw_L = true;
-      }
-      else
-      {
-        sw_L = false;
-      }
-      if (sw_L && !sw_R)
-      {
-        line = 'E';
-      }
-      else if (!sw_L && !sw_R)
-      {
-        line = 'A';
-      }
-      else if (!sw_L && sw_R)
-      {
-        line = 'D';
-      }
-      else if (sw_L && sw_R)
-      {
-        line = 'G';
-      }
-    }
-    loopCount++; 
-  }
-
-  Wire.requestFrom(0x08, 4); // I2Cデバイスから4バイトのデータを要求
-  loopCount = 0;
-  while (Wire.available())
-  {                       
-    uint8_t c = Wire.read(); // 1バイト読み取る
-    if (loopCount == 0 && line == 'G')
-    {
-      if (c >= 0b10000 && c < 0b100000)
-      {
-        toneNum = NOTE_GS6;
-      }
-      else if (c >= 0b100000 && c < 0b1000000)
-      {
-        toneNum = NOTE_A6;
-      }
-      else if (c >= 0b1000000 && c < 0b10000000)
-      {
-        toneNum = NOTE_AS6;
-      }
-      else if (c >= 0b10000000 && c < 0b100000000)
-      {
-        toneNum = NOTE_B6;
-      }
-      else if (c == 0)
-      {
-        toneNum = NOTE_G6;
-      }
-    }
-    else if (loopCount == 1 && line == 'G')
-    {
-      if (c == 0b1)
-      {
-        toneNum = NOTE_C7;
-      } //else if (c == 0) {
-      //   toneNum = NOTE_G6;
-      // }
-    }
-    else if (loopCount == 1 && line == 'D')
-    {
-      if (c >= 0b10 && c < 0b100)
-      {
-        toneNum = NOTE_DS6;
-      }
-      else if (c >= 0b100 && c < 0b1000)
-      {
-        toneNum = NOTE_E6;
-      }
-      else if (c >= 0b1000 && c < 0b10000)
-      {
-        toneNum = NOTE_F6;
-      }
-      else if (c >= 0b10000 && c < 0b100000)
-      {
-        toneNum = NOTE_FS6;
-      }
-      else if (c >= 0b100000 && c < 0b1000000)
-      {
-        toneNum = NOTE_G6;
-      }
-      else if (c == 0)
-      {
-        toneNum = NOTE_D6;
-      }
-    }
-    else if (loopCount == 1 && line == 'A')
-    {
-      if (c >= 0b1000000 && c < 0b10000000)
-      {
-        toneNum = NOTE_AS6;
-      }
-      else if (c >= 0b10000000 && c < 0b100000000)
-      {
-        toneNum = NOTE_B6;
-      }
-      else if (c == 0)
-      {
-        toneNum = NOTE_A6;
-      }
-    }
-    else if (loopCount == 2 && line == 'A')
-    {
-      if (c == 0b1)
-      {
-        toneNum = NOTE_C5;
-      }
-      else if (c >= 0b10 && c < 0b100)
-      {
-        toneNum = NOTE_CS5;
-      }
-      else if (c >= 0b100 && c < 0b1000)
-      {
-        toneNum = NOTE_D5;
-      }
-      // else if (c == 0)
-      // {
-      //   toneNum = NOTE_A5;
-      // }
-    }
-    else if (loopCount == 2 && line == 'E')
-    {
-      if (c >= 0b1000 && c < 0b10000) // 4ビット目をチェック
-      {
-        toneNum = NOTE_F5;
-      }
-      else if (c >= 0b10000 && c < 0b100000) // 5ビット目をチェック
-      {
-        toneNum = NOTE_FS5;
-      }
-      else if (c >= 0b100000 && c < 0b1000000) // 6ビット目をチェック
-      {
-        toneNum = NOTE_G5;
-      }
-      else if (c >= 0b1000000 && c < 0b10000000) // 7ビット目をチェック
-      {
-        toneNum = NOTE_GS5;
-      }
-      else if (c == 0)
-      {
-        toneNum = NOTE_E5;
-      }
-    }
-    else if (loopCount == 3 && line == 'E')
-    {
-      if (c == 0b100)
-      {
-        toneNum = NOTE_A5;
-      }
-      else
-      {
-        toneNum = toneNum;
-      }
-      // else if (c == 0)
-      // {
-      //   toneNum = NOTE_E5;
-      // }
-    }
-    loopCount++;
-  }
-
-  if (toneNum != lasttoneNum)
-  {
-    USBSerial.println(toneNum); // シリアルモニタに表示
-    synth.setInstrument(0, 0, instrument);
-    synth.setNoteOff(0, lasttoneNum, 0);
-
-    if (toneNum != 0)
-    {
-      synth.setNoteOn(0, toneNum, 127);
-    }
-    lasttoneNum = toneNum;
-  }
-  // USBSerial.println(toneNum); // シリアルモニタに表示
-  USBSerial.println(line);
-  USBSerial.println(toneNum);
-  delay(100);         // 1秒間の遅延
+  // line = 'E'; //'G','D', 'A', 'E'
+  // // 1/100秒で変わることはちょっと忘れさせてください
+  // Wire.requestFrom(0x08, 4);
+  // uint8_t loopCount = 0;
+  // while (Wire.available())
+  // {
+  //   uint8_t c = Wire.read();
+  //   if (loopCount == 3)
+  //   {
+  //     USBSerial.println(c, BIN);
+  //     if ((c & 0b1000) == 0b1000)
+  //     {
+  //       sw_R = true;
+  //     } 
+  //     else {
+  //       sw_R = false;
+  //     }
+  //     if (c >= 0b10000 && c < 0b100000)
+  //     {
+  //       sw_L = true;
+  //     }
+  //     else
+  //     {
+  //       sw_L = false;
+  //     }
+  //     if (sw_L && !sw_R)
+  //     {
+  //       line = 'E';
+  //     }
+  //     else if (!sw_L && !sw_R)
+  //     {
+  //       line = 'A';
+  //     }
+  //     else if (!sw_L && sw_R)
+  //     {
+  //       line = 'D';
+  //     }
+  //     else if (sw_L && sw_R)
+  //     {
+  //       line = 'G';
+  //     }
+  //   }
+  //   loopCount++; 
+  // }
 }
+
